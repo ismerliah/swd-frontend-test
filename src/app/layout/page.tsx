99a14095e2844c55b7efa811962375add5c5eb62
Yanisa Thaenthong <yanisa.thaenthong@gmail.com>
2025-07-19 01:24:52 +0700

    feat: create layout page and provide all shape

---
+++ src/app/layout/page.tsx
@@ -0,0 +1,82 @@
+import { Card, Col, Divider, Flex, Row } from "antd";
+import Title from "antd/es/typography/Title";
+import styles from "./page.module.scss";
+import React from "react";
+
+type Props = {};
+
+export default function LayoutPage({}: Props) {
+  return (
+    <div className="page">
+      <header>
+        <Title level={2}>Layout & Style</Title>
+      </header>
+      <main className={styles.main}>
+        <Row justify="center" gutter={16}>
+          <Col span={7}>
+            <Card className={styles.card}>
+              <div className={styles["shape-triangle-left"]}></div>
+              <div className={styles.badge}>Move Shape</div>
+            </Card>
+          </Col>
+          <Col span={10}>
+            <Card className={styles.card}>
+              <div className={styles["shape-column"]}>
+                <div className={styles["shape-triangle-up"]}></div>
+                <div className={styles["shape-triangle-down"]}></div>
+              </div>
+
+              <div className={styles.badge}>Move Position</div>
+            </Card>
+          </Col>
+
+          <Col span={7}>
+            <Card className={styles.card}>
+              <div className={styles["shape-triangle-right"]}></div>
+              <div className={styles.badge}>Move Shape</div>
+            </Card>
+          </Col>
+        </Row>
+
+        <Divider size="middle" />
+
+        <Flex gap="middle" vertical>
+          <Row justify="center" gutter={16}>
+            <Col span={7}>
+              <Card className={styles.card}>
+                <div className={styles["shape-square"]}></div>
+              </Card>
+            </Col>
+            <Col span={7}>
+              <Card className={styles.card}>
+                <div className={styles["shape-circle"]}></div>
+              </Card>
+            </Col>
+            <Col span={7}>
+              <Card className={styles.card}>
+                <div className={styles["shape-oval"]}></div>
+              </Card>
+            </Col>
+          </Row>
+          <Row justify="end" gutter={16}>
+            <Col span={7}>
+              <Card className={styles.card}>
+                <div className={styles["shape-trapezoid"]}></div>
+              </Card>
+            </Col>
+            <Col span={7}>
+              <Card className={styles.card}>
+                <div className={styles["shape-rectangle"]}></div>
+              </Card>
+            </Col>
+            <Col span={7}>
+              <Card className={styles.card}>
+                <div className={styles["shape-parallelogram"]}></div>
+              </Card>
+            </Col>
+          </Row>
+        </Flex>
+      </main>
+    </div>
+  );
+}
