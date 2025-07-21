4fdeb669c7676e7070e0c4823868c29929fab956
Yanisa Thaenthong <yanisa.thaenthong@gmail.com>
2025-07-21 09:48:02 +0700

    fix: resolve value not update in language switcher

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,22 +1,24 @@
 import { Select } from "antd";
 import i18next from "i18next";
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function LanguageSwitcher() {
   const { t } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState(i18next.language);
   const handleLanguageChange = (newLanguage: string) => {
     i18next.changeLanguage(newLanguage);
+    setSelectedLanguage(newLanguage);
   };
   return (
     <div>
       <Select
-        defaultValue="en"
+        value={selectedLanguage}
         style={{ width: 100 }}
         onChange={handleLanguageChange}
         options={[
-          { value: "en", label: `${t("EN")}` },
-          { value: "th", label: `${t("TH")}` },
+          { value: "en", label: t("EN", { lng: i18next.language }) },
+          { value: "th", label: t("TH", { lng: i18next.language }) },
         ]}
       />
     </div>
